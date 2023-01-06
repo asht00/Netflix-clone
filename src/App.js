cc7e338bc60e750e2d09b03f8c932cd093a49756
Ashima Tripathi <ashima@go-gaga-app.com>
2023-01-06 19:55:19 +0530

    Initial commit:Homescreen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import React from 'react';
 import './App.css';
-import HomeScreen from './HomeScreen';
+import HomeScreen from './screens/HomeScreen';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import LoginScreen from './screens/LoginScreen';
 
 function App() {
+  const user = null;
+
   return (
     <div className="app">
-      <HomeScreen />
+      <Router>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+            <Routes>
+              <Route exact path='/' element={<HomeScreen />}>
+                
+              </Route>
+            </Routes>
+        )}
+      </Router>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDZqDcdqWos1_et65uv3kcGWpk567lDDWc",
+    authDomain: "netflix-clone-ec462.firebaseapp.com",
+    projectId: "netflix-clone-ec462",
+    storageBucket: "netflix-clone-ec462.appspot.com",
+    messagingSenderId: "1056503770648",
+    appId: "1:1056503770648:web:6845ba2ed3c70cad31dd6d",
+    measurementId: "G-QG3HYN3T0V"
+  };
